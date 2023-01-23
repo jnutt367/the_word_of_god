34ac8d60cd657f6dd775b0cf50a8d3f198b4b869
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-22 21:28:29 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -119,28 +119,6 @@ export default function Home() {
               </div>
           </div>
  </main>
-                       <p className={styles.description}>
-          
-          <code className={styles.publisher}>Coded, Published & Deployed by His servant: Jason Daniel Nutt </code>
-        </p>
-
-     
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    
-     
-
     </div>
   )
 }
