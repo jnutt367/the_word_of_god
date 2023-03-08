ce7898222b9dc3a9c079a61f9a158280a0b34186
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-03-07 19:32:11 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -137,9 +137,7 @@ export default function Home() {
                        <a className={styles.proverbs} href="https://gofund.me/74b25232">
                          GoFundThisBibleApp
                          </a>
-<div class="gfm-embed" data-url="https://www.gofundme.com/f/the-holy-bible-transcribed-by-jason-nutt/widget/small">
-  
-  </div>
+
 
 <script defer src="https://www.gofundme.com/static/js/embed.js"></script>
                <p className={styles.hide}> understanding proverbs and parables,
