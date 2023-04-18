acf28e732c8cf4e22ce1bd86e0cb4b641e331d2a
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-04-18 18:13:20 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,10 +44,15 @@ export default function Home() {
  
 <iframe width="100%" height="200" src="https://www.youtube.com/embed/J0p9Qp1cyfk" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                       <div className={styles.nav}>           
-     
+       <a href="#">
+            <p className={styles.donate}>
+              SCROLL DOWN  < br />
+              
+              </p>
+       </a>           
          <a href="https://gofund.me/754323ae">
-           <Image className={styles.donate}
-                                src="/funding.png"
+           <Image
+                                src="/fundme.png"
                                 alt="gofundme-icon"
                                 height={75}
                                 width={75}
