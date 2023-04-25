4b981d5feeeaff9711a8cba71f068f85b4b792e0
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-04-25 12:15:41 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -77,6 +77,8 @@ export default function Home() {
  
 
 
+  <div id="fb-root"></div>
+  <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
 
 
 <iframe width="100%" height="250" src="https://fb.watch/k7HXfWIJ5X/" title="YouTube video player" frameborder="2" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
