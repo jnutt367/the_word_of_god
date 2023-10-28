6ff1733c137b0525f78929daa16f6ee9be483840
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-10-28 00:23:26 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,10 +29,9 @@ export default function Home() {
                     RISEN
              </p>
                          
-                              <div  className={styles.iframe1}>
-                   <iframe width="100%" height="315" src="https://www.youtube.com/embed/cj9jBcaNqvA?si=VASbdQ1yI1SM0Ub9?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    
-    </div>
+                            <div  className={styles.iframe1}>
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/Dgeyy7CbxuM?si=bP7u7Scb24ZW7g3e?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                              </div>
                               
 
 
@@ -542,9 +541,7 @@ This is a video of the apps humble beginnings, well. this was about 5 months int
 <iframe width="100%" height="315" src="https://www.youtube.com/embed/KSpB0un2S8g?si=UuJaUiMPkr9Paexd?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 
- <div  className={styles.iframe1}>
-<iframe width="100%" height="315" src="https://www.youtube.com/embed/Dgeyy7CbxuM?si=bP7u7Scb24ZW7g3e?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                              </div>
+ 
                               
                                   <p className={styles.glory}>"All glory to GOD, who is able, through His mighty power at work within us, to accomplish infinitely more than we ask or imagine"  Ephesians 3:20
                                   </p> 
