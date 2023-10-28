d2769393844779bfe79d13e8d88be7174d5c75c6
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-10-28 00:38:02 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,11 +30,9 @@ export default function Home() {
              </p>
                          
                             <div  className={styles.iframe1}>
-<iframe width="100%" height="315" src="https://www.youtube.com/embed/KSpB0un2S8g?si=jklyBpFWU0Z7Nl2z?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                              </div>
-                              
-
-
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/7_CGP-12AE0?si=H9PXCAy3UWNQRTCJ?autoplay=1&mute=1" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
+          
  </div>               
   <p className={styles.parables}>
          God was, God is & God forever will be...the long awaited messiah, King Jesus! And He has RISEN!                               
