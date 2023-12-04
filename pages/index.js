bfaf0757df2103140c31d07815ed15e778679e2d
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-03 21:40:23 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,11 @@ export default function Home() {
 <link
           href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap, Montserrat,Roboto"
           rel="stylesheet"
-        />        
+        />   
+<link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400..700&display=swap"
+          rel="stylesheet"
+        />                   
         <title>The WORD of GOD: "Risen"</title>
       <meta
   name="description"
