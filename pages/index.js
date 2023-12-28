38aedc62d6d73288196850e28097baf402a5a0da
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-28 13:46:46 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
           <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="google-site-verification" content="AN98PURzX6yTs2oHSNkBULbkybRDCPd6kbkLseVLpME" />    
  <link href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css" rel="stylesheet" />
   <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400..700&display=swap, Montserrat,Roboto"
